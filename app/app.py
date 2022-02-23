d0f43855b7f8d3da0f3a3720617068ef5282d27a
peterlee-bolttech <84845611+peterlee-bolttech@users.noreply.github.com>
2022-02-24 01:31:56 +0800

    Feat/develop (#6)
    * created data folder
    
    * modified data path
    
    Co-authored-by: Peter Lee <peterlee@Rachels-MacBook-Pro-2.local>

--- app/app.py
+++ app/app.py
@@ -35,7 +35,8 @@ ab_default = None
 result_default = None
 
 if use_prior_example_file:
-    prior_file = "../data/prior_data.csv"
+  
+    prior_file = "./data/prior_data.csv"
     ab_default = ['group']
     result_default = ['converted']
 
@@ -98,7 +99,8 @@ if prior_file:
     )
 
     if use_posterior_example_file:
-        posterior_file = "../data/experiment_data.csv"
+
+        posterior_file = "./data/experiment_data.csv"
         ab_default = ['group']
         result_default = ['converted']
 
